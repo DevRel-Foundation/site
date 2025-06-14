0455da793a53633387dbcd71bbf36ed873797c6c
ladyofcode <thelab@ladyofcode.com>
2025-06-14 12:33:49 +0100

    Add nav markup

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,11 @@
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/team">Team</a></li>
+    <li><a href="/events">Events</a></li>
+    <li><a href="/join-us">Join us</a></li>
+    <li><a href="/working-groups">Working groups</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav> 
