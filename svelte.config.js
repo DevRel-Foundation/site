8ddf53ac62f21e555da9160a66d0694203058e43
Ana Jimenez Santamaria <ana@todogroup.org>
2025-06-15 06:48:14 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,16 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : './'
+		}
+	},
 	extensions: ['.svelte', '.svx']
 };
 
