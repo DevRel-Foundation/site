bf8cfb2a62acadf5ae1a264885776e3f0554b2c9
j12y <jayson.delancey@gmail.com>
2025-07-14 17:09:54 -0700

    dropdown menu navigation and path updates to allow for more content to be added

---
+++ src/routes/about/mission/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import BioSteeringCommittee from '$lib/BioSteeringCommittee.svelte';
+	const img_wes = '/images/steering-committee/wes_f.jpg';
+	const img_ana = '/images/steering-committee/ana_j.jpg';
+	const img_jon = '/images/steering-committee/jon_l.jpg';
+	const img_kat = '/images/steering-committee/katie_m.jpg';
+	const img_phil = '/images/steering-committee/phil_l.jpg';
+	const img_stacey = '/images/steering-committee/stacey_k.jpg';
+</script>
+
+<svelte:head>
+	<title>About - Developer Relations Foundation</title>
+</svelte:head>
+
+<div class="container container-content">
+	<section class="mission-section">
+		<h1 id="about">Our Mission</h1>
+		<p>
+			<strong>Mission:</strong> To elevate the professional practice of developer relations and increase
+			awareness of it as a driver of business value.
+		</p>
+	</section>
+
+</div>
+
+<style>
+  .container {
+    margin-bottom: var(--space-l);
+  }
+
+</style>
