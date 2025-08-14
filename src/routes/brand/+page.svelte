bb8eba59042a021ef0ebc397c6e78985894766b3
j12y <jayson.delancey@gmail.com>
2025-08-13 19:48:05 -0700

    added additional brand guidance

--- src/routes/brand/+page.svelte
+++ src/routes/brand/+page.svelte
@@ -1,8 +1,58 @@
 
+
 <div class="container container-content">
 
+  <h1>Brand Guidelines</h1>
+  <p>
+    Representing the DevRel Foundation brand in public should be done with care and consistency. Please reach out and
+    <a href="/contact">contact us</a> for permission and guidance on using brand assets.
+  </p>
 
-<h2>Colors</h2>
+  <nav class="toc">
+  <ul>
+    <li><a href="#logos">Logos</a></li>
+    <li><a href="#colors">Colors</a></li>
+    <li><a href="#usage">Color Usage</a></li>
+    <li><a href="#usage">Typography</a></li>
+    <li><a href="#ip">License and Intellectual Property</a></li>
+  </ul>
+</nav>
+
+
+<h2 id="usage">Logos</h2>
+<p>
+    Download <a href="https://github.com/DevRel-Foundation/drf-branding" target="_blank" rel="noopener noreferrer">Brand Assets ↗</a> from the GitHub repository.
+</p>
+
+<table class="brand-colors-table">
+  <thead>
+  </thead>
+  <tbody>
+    <tr>
+      <td style="background: var(--color-offwhite);">
+        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo-dark-text.svg" alt="logo-dark-text.svg" width="300"/>
+        logo-dark-text.svg
+      </td>
+      <td style="background: var(--color-blue-dark);">
+        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo-light-text.svg" alt="logo-light-text.svg" width="300"/>
+        <span style="color: var(--color-text-dark);">logo-light-text.svg</span>
+      </td>
+    </tr>
+    <tr>
+      <td style="background: var(--color-offwhite);">
+        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo.svg" alt="logo.svg" width="75"/>
+        logo.svg
+      </td>
+      <td style="background: var(--color-blue-dark);">
+        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo-gray.png" alt="logo-gray.png" width="300"/>
+        <span style="color: var(--color-text-dark);">logo-gray.png</span>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
+<h2 id="colors">Colors</h2>
 <p>This is the brand color palette for the DevRel Foundation. It includes all the primary and secondary colors used throughout the website.</p>
 
 <table class="brand-colors-table">
@@ -18,81 +68,81 @@
       <th>HSL</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td>--color-offwhite</td>
-      <td><span class="color-swatch" style="background: var(--color-offwhite);"></span></td>
-      <td></td>
-      <td></td>
-      <td>--color-offwhite-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-offwhite-dark);"></span></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>--color-cream</td>
-      <td><span class="color-swatch" style="background: var(--color-cream);"></span></td>
-      <td></td>
-      <td></td>
-      <td>--color-cream-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-cream-dark);"></span></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>--color-gold</td>
-      <td><span class="color-swatch" style="background: var(--color-gold);"></span></td>
-      <td></td>
-      <td></td>
-      <td>--color-gold-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-gold-dark);"></span></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>--color-mint</td>
-      <td><span class="color-swatch" style="background: var(--color-mint);"></span></td>
-      <td>#5FFFC7</td>
-      <td>hsl(90, 65%, 81%)</td>
-      <td>--color-mint-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-mint-dark);"></span></td>
-      <td>#142F03</td>
-      <td>hsl(90, 95%, 8%)</td>
-    </tr>
-    <tr>
-      <td>--color-sage</td>
-      <td><span class="color-swatch" style="background: var(--color-sage);"></span></td>
-      <td></td>
-      <td></td>
-      <td>--color-sage-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-sage-dark);"></span></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>--color-emerald</td>
-      <td><span class="color-swatch" style="background: var(--color-emerald);"></span></td>
-      <td></td>
-      <td></td>
-      <td>--color-emerald-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-emerald-dark);"></span></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>--color-blue</td>
-      <td><span class="color-swatch" style="background: var(--color-blue);"></span></td>
-      <td></td>
-      <td></td>
-      <td>--color-blue-dark</td>
-      <td><span class="color-swatch" style="background: var(--color-blue-dark);"></span></td>
-      <td></td>
-      <td></td>
-    </tr>
-  </tbody>
+<tbody>
+  <tr>
+    <td>--color-offwhite</td>
+    <td><span class="color-swatch" style="background: var(--color-offwhite);"></span></td>
+    <td>#FAFAF0</td>
+    <td>hsl(48, 100%, 98%)</td>
+    <td>--color-offwhite-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-offwhite-dark);"></span></td>
+    <td>#23313A</td>
+    <td>hsl(192, 32%, 22%)</td>
+  </tr>
+  <tr>
+    <td>--color-cream</td>
+    <td><span class="color-swatch" style="background: var(--color-cream);"></span></td>
+    <td>#F8F3E6</td>
+    <td>hsl(48, 61.8%, 94%)</td>
+    <td>--color-cream-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-cream-dark);"></span></td>
+    <td>#463A2C</td>
+    <td>hsl(42, 32%, 24%)</td>
+  </tr>
+  <tr>
+    <td>--color-gold</td>
+    <td><span class="color-swatch" style="background: var(--color-gold);"></span></td>
+    <td>#F9E5C1</td>
+    <td>hsl(41.9, 100%, 91.6%)</td>
+    <td>--color-gold-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-gold-dark);"></span></td>
+    <td>#6B4F1B</td>
+    <td>hsl(41.7, 74.5%, 21.6%)</td>
+  </tr>
+  <tr>
+    <td>--color-mint</td>
+    <td><span class="color-swatch" style="background: var(--color-mint);"></span></td>
+    <td>#5FFFC7</td>
+    <td>hsl(90, 65%, 81%)</td>
+    <td>--color-mint-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-mint-dark);"></span></td>
+    <td>#142F03</td>
+    <td>hsl(90, 95%, 8%)</td>
+  </tr>
+  <tr>
+    <td>--color-sage</td>
+    <td><span class="color-swatch" style="background: var(--color-sage);"></span></td>
+    <td>#D9EAD3</td>
+    <td>hsl(120, 20%, 80%)</td>
+    <td>--color-sage-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-sage-dark);"></span></td>
+    <td>#334D33</td>
+    <td>hsl(120, 20%, 20%)</td>
+  </tr>
+  <tr>
+    <td>--color-emerald</td>
+    <td><span class="color-swatch" style="background: var(--color-emerald);"></span></td>
+    <td>#26B36B</td>
+    <td>hsl(140, 60%, 40%)</td>
+    <td>--color-emerald-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-emerald-dark);"></span></td>
+    <td>#145C36</td>
+    <td>hsl(140, 60%, 20%)</td>
+  </tr>
+  <tr>
+    <td>--color-blue</td>
+    <td><span class="color-swatch" style="background: var(--color-blue);"></span></td>
+    <td>#E6F7FA</td>
+    <td>hsl(193, 100%, 92%)</td>
+    <td>--color-blue-dark</td>
+    <td><span class="color-swatch" style="background: var(--color-blue-dark);"></span></td>
+    <td>#00242E</td>
+    <td>hsl(192, 100%, 9%)</td>
+  </tr>
+</tbody>
 </table>
 
-<h3>Usage</h3>
+<h3 id="usage">Usage</h3>
 <p>Semantic use of the brand colors.</p>
 
 <table class="brand-colors-table">
@@ -100,12 +150,10 @@
     <tr>
       <th>Variable</th>
       <th>Swatch</th>
-      <th>Hex</th>
-      <th>HSL</th>
+      <th>Color</th>
       <th>Dark Mode</th>
       <th>Swatch</th>
-      <th>Hex</th>
-      <th>HSL</th>
+      <th>Color</th>
     </tr>
   </thead>
   <tbody>
@@ -113,113 +161,105 @@
       <td>--color-background</td>
       <td><span class="color-swatch" style="background: var(--color-background);"></span></td>
       <td>offwhite</td>
-      <td></td>
       <td>--color-background-dark</td>
       <td><span class="color-swatch" style="background: var(--color-background-dark);"></span></td>
       <td>blue-dark</td>
-      <td></td>
     </tr>
     <tr>
       <td>--color-background-secondary-1</td>
       <td><span class="color-swatch" style="background: var(--color-background-secondary-1);"></span></td>
       <td>cream</td>
-      <td></td>
       <td>--color-background-secondary-1-dark</td>
       <td><span class="color-swatch" style="background: var(--color-background-secondary-1-dark);"></span></td>
       <td>offwhite-dark</td>
-      <td></td>
     </tr>
     <tr>
       <td>--color-background-secondary-2</td>
       <td><span class="color-swatch" style="background: var(--color-background-secondary-2);"></span></td>
       <td>gold</td>
-      <td></td>
       <td>--color-background-secondary-2-dark</td>
       <td><span class="color-swatch" style="background: var(--color-background-secondary-2-dark);"></span></td>
       <td>mint-dark</td>
-      <td></td>
     </tr>
     <tr>
       <td>--color-text</td>
       <td><span class="color-swatch" style="background: var(--color-text);"></span></td>
       <td>mint-dark</td>
-      <td></td>
       <td>--color-text-dark</td>
       <td><span class="color-swatch" style="background: var(--color-text-dark);"></span></td>
       <td>offwhite</td>
-      <td></td>
     </tr>
     <tr>
       <td>--color-link</td>
       <td><span class="color-swatch" style="background: var(--color-link);"></span></td>
       <td><a href="#">emerald-dark</a></td>
-      <td></td>
       <td>--color-link-dark</td>
       <td><span class="color-swatch" style="background: var(--color-link-dark);"></span></td>
       <td><a href="#">cream</a></td>
-      <td></td>
     </tr>
     <tr>
       <td>--color-accent-text</td>
       <td><span class="color-swatch" style="background: var(--color-accent-text);"></span></td>
       <td>mint</td>
-      <td></td>
       <td>--color-accent-text-dark</td>
       <td><span class="color-swatch" style="background: var(--color-accent-text-dark);"></span></td>
       <td>sage</td>
-      <td></td>
     </tr>
     <tr>
       <td>--color-logo-text</td>
       <td><span class="color-swatch" style="background: var(--color-logo-text);"></span></td>
       <td>blue-dark</td>
-      <td></td>
       <td>--color-logo-text-dark</td>
       <td><span class="color-swatch" style="background: var(--color-logo-text-dark);"></span></td>
       <td>mint</td>
-      <td></td>
     </tr>
   </tbody>
 </table>
 
-<h2>Logos</h2>
-<p>
-    Find <a href="https://github.com/DevRel-Foundation/drf-branding" target="_blank" rel="noopener noreferrer">Brand Assets ↗</a> in the GitHub repository.
-</p>
+<h2 id="usage">Typography</h2>
 
-<table class="brand-colors-table">
-  <thead>
-  </thead>
-  <tbody>
-    <tr>
-      <td>
-        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo-dark-text.svg" alt="logo-dark-text.svg" width="300"/>
-        logo-dark-text.svg
-      </td>
-      <td style="background: var(--color-blue-dark);">
-        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo-light-text.svg" alt="logo-light-text.svg" width="300"/>
-        <span style="color: var(--color-text-dark);">logo-light-text.svg</span>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo.svg" alt="logo.svg" width="75"/>
-        logo.svg
-      </td>
-      <td style="background: var(--color-blue-dark);">
-        <img src="https://raw.githubusercontent.com/DevRel-Foundation/drf-branding/dev/j12y/organize/assets/logos/logo-gray.png" alt="logo-gray.png" width="300"/>
-        <span style="color: var(--color-text-dark);">logo-gray.png</span>
-      </td>
-    </tr>
-  </tbody>
-</table>
+
+
+  <h2 id="ip">License and Intellectual Property</h2>
+
+  <p>
+    All documentation is made available by the Developer Relations Foundation under the 
+    <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">
+      Creative Commons Attribution 4.0 International License
+    </a>.
+  </p>
+
+  <p>
+    This means that anyone contributing to or using this documentation—including managers, working groups, and users—must follow the terms of this license. In simple terms:
+  </p>
+
+  <ul>
+    <li>You are free to share and adapt the content (copy, redistribute, remix, transform)</li>
+    <li>But <strong>you must give proper credit to the Developer Relations Foundation</strong></li>
+    <li>You also need to indicate if changes were made</li>
+  </ul>
+
+  <br/>
+  <p><b>How to reference if reusing the content as-is (without modifications):</b></p>
+  <blockquote>
+    This documentation includes material from the Developer Relations Foundation, available under the 
+    <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">
+      Creative Commons Attribution 4.0 International License
+    </a>.
+  </blockquote>
+
+  <br/>
+  <p><b>How to reference adapting or modifying the content:</b></p>
+  <blockquote>
+    This documentation is adapted from materials originally created by the Developer Relations Foundation, available under the 
+    <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">
+      Creative Commons Attribution 4.0 International License
+    </a>. Changes have been made from the original.
+  </blockquote>
 
 
 
 </div>
-
-
-
 
 <style>
 
@@ -258,4 +298,29 @@
   vertical-align: middle;
   margin-right: 0.5rem;
 }
+
+.toc {
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+}
+
+.toc ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 2rem;
+}
+
+.toc a {
+  color: var(--color-link);
+  text-decoration: underline;
+  font-weight: 600;
+  font-size: 1rem;
+}
+
+.toc a:hover {
+  color: var(--color-link);
+}
+
 </style>
